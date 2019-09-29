89164b6efebcab845c0ba6975973e35d53b9304b
RootenberG <savichevdenis244@gmail.com>
2019-09-29 17:32:43 +0300

    Initial commit

---
+++ github_bot/__main__.py
@@ -0,0 +1,9 @@
+from octomachinery.app.server.runner import run as run_app
+
+
+if __name__ == "__main__":
+    run_app(
+        name='first-rootenberg-bot',
+        version='1.0.0',
+        url='https://github.com/settings/apps/first-rootenberg-bot',
+    )
